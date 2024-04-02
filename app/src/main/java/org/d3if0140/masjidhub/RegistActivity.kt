dc6b1e7bad84967ff03bf12715f33f238494b002
adamrza <adamlabibmrza@gmail.com>
2024-04-02 19:01:38 +0700

    konversi ke viewBinding

--- app/src/main/java/org/d3if0140/masjidhub/RegistActivity.kt
+++ app/src/main/java/org/d3if0140/masjidhub/RegistActivity.kt
@@ -3,6 +3,7 @@ package org.d3if0140.masjidhub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
 import android.text.SpannableString
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
@@ -12,83 +13,71 @@ import android.widget.ImageButton
 import android.widget.Spinner
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
+import org.d3if0140.masjidhub.databinding.ActivityRegistBinding
 
 class RegistActivity : AppCompatActivity() {
-
+    // Deklarasi variabel binding untuk menggunakan ViewBinding
+    private lateinit var binding: ActivityRegistBinding
     private lateinit var passwordEditText: TextInputEditText
     private lateinit var passwordToggle: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_regist)
+        // Menggunakan inflate pada binding untuk menghubungkan layout XML dengan Activity
+        binding = ActivityRegistBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val backButton: ImageButton = findViewById(R.id.backButton)
+        // Menambahkan onClickListener pada button backButton untuk kembali ke WelcomeActivity
+        val backButton: ImageButton = binding.backButton
         backButton.setOnClickListener {
-            val intent = Intent(this,WelcomeActivity::class.java)
+            val intent = Intent(this, WelcomeActivity::class.java)
             startActivity(intent)
         }
 
-        val textLogin: TextView = findViewById(R.id.textLogin)
-
-        // Teks yang akan ditampilkan dalam TextView
+        // Mengatur teks pada textLogin menjadi "Sudah punya akun? Login" dengan tautan ke LoginActivity
+        val textLogin: TextView = binding.textLogin
         val text = "Sudah punya akun? Login"
-
-        // Membuat objek SpannableString dari teks
         val spannableString = SpannableString(text)
-
-        // Mendapatkan indeks kata "sini" dalam teks
         val startIndex = text.indexOf("Login")
-
-        // Membuat ClickableSpan untuk menangani klik pada kata "sini"
         val clickableSpan = object : ClickableSpan() {
             override fun onClick(view: View) {
-                // Tindakan yang akan dilakukan saat teks diklik (mengarahkan ke halaman pendaftaran)
                 val intent = Intent(this@RegistActivity, LoginActivity::class.java)
                 startActivity(intent)
             }
         }
-
-        // Menambahkan ClickableSpan ke SpannableString
         spannableString.setSpan(clickableSpan, startIndex, startIndex + 4, SpannableString.SPAN_EXCLUSIVE_EXCLUSIVE)
-
-        // Mengatur teks dalam TextView
         textLogin.text = spannableString
-
-        // Mengaktifkan tautan gerak dalam TextView
         textLogin.movementMethod = LinkMovementMethod.getInstance()
 
-        passwordEditText = findViewById(R.id.passwordEditText)
-        passwordToggle = findViewById(R.id.passwordToggle)
+        // Menghubungkan passwordEditText dan passwordToggle dengan elemen UI yang sesuai
+        passwordEditText = binding.passwordEditText
+        passwordToggle = binding.passwordToggle
 
-        // Set onClickListener for passwordToggle button
+        // Menambahkan onClickListener pada passwordToggle untuk mengubah visibilitas password
         passwordToggle.setOnClickListener {
-            // Change inputType of passwordEditText to toggle visibility
             val inputType = passwordEditText.inputType
-            if (inputType == 129) { // Password mode (textPassword)
-                passwordEditText.inputType = 145 // Visible password mode (textVisiblePassword)
-                passwordToggle.setImageResource(R.drawable.baseline_visibility) // Change icon to visible
-            } else { // Visible password mode (textVisiblePassword)
-                passwordEditText.inputType = 129 // Password mode (textPassword)
-                passwordToggle.setImageResource(R.drawable.baseline_visibility_off) // Change icon to hidden
+            if (inputType == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
+                passwordEditText.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                passwordToggle.setImageResource(R.drawable.baseline_visibility_off)
+            } else {
+                passwordEditText.inputType = InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                passwordToggle.setImageResource(R.drawable.baseline_visibility)
             }
-            // Move cursor to the end of the text
             passwordEditText.setSelection(passwordEditText.text?.length ?: 0)
         }
 
+        // Menghubungkan emailEditText dan spinner dengan elemen UI yang sesuai
+        val emailEditText: TextInputEditText = binding.emailEditText
+        val spinner: Spinner = binding.DkmSpinner
 
-        val emailEditText: TextInputEditText = findViewById(R.id.emailEditText)
-        val spinner: Spinner = findViewById(R.id.DkmSpinner)
-
-        // Data untuk opsi dropdown
+        // Data untuk opsi dropdown pada spinner
         val dkmOptions = arrayOf("Jamaah Masjid", "Masjid Nurul Hikmah", "Masjid Al-Lathif", "Masjid Al-Jabar")
 
-        // Buat adapter untuk spinner
+        // Membuat adapter untuk spinner dan mengatur posisi awal ke "Jamaah Masjid"
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, dkmOptions)
-
-        // Set adapter untuk spinner
         spinner.adapter = adapter
-
-        // Atur posisi awal spinner ke "Jamaah Masjid"
         spinner.setSelection(0)
-
     }
 }
+
--- app/src/main/java/org/d3if0140/masjidhub/WelcomeActivity.kt
+++ app/src/main/java/org/d3if0140/masjidhub/WelcomeActivity.kt
@@ -3,22 +3,28 @@ package org.d3if0140.masjidhub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import org.d3if0140.masjidhub.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
+    // Deklarasi variabel binding untuk menggunakan ViewBinding
+    private lateinit var binding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        // Menggunakan inflate pada binding untuk menghubungkan layout XML dengan Activity
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val loginButton = findViewById<Button>(R.id.loginButton)
-        loginButton.setOnClickListener {
-            val intent = Intent(this,LoginActivity::class.java)
+        // Menambahkan onClickListener pada button login untuk memulai LoginActivity
+        binding.loginButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
 
-        val registerButton = findViewById<Button>(R.id.registerButton)
-        registerButton.setOnClickListener {
-            val intent = Intent(this,RegistActivity::class.java)
+        // Menambahkan onClickListener pada button register untuk memulai RegistActivity
+        binding.registerButton.setOnClickListener {
+            val intent = Intent(this, RegistActivity::class.java)
             startActivity(intent)
         }
     }
