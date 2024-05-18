5ecd76851746c0e636c4528013044d284e731ae4
ardiyamalik <91653831+ardiyamalik@users.noreply.github.com>
2024-05-18 08:42:54 +0700

    tetap login sesuai role

--- app/src/main/java/org/d3if0140/masjidhub/WelcomeActivity.kt
+++ app/src/main/java/org/d3if0140/masjidhub/WelcomeActivity.kt
@@ -4,11 +4,14 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import org.d3if0140.masjidhub.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
     // Deklarasi variabel binding untuk menggunakan ViewBinding
     private lateinit var binding: ActivityWelcomeBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +19,9 @@ class WelcomeActivity : AppCompatActivity() {
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
 
         // Memeriksa apakah pengguna sudah login sebelumnya
         val currentUser = FirebaseAuth.getInstance().currentUser
@@ -44,8 +50,26 @@ class WelcomeActivity : AppCompatActivity() {
     }
 
     private fun navigateToHome() {
-        val intent = Intent(this, HomeActivity::class.java)
-        startActivity(intent)
-        finish() //agar pengguna tidak dapat kembali ke halaman welcome dengan menekan tombol back
+        val userId = auth.currentUser?.uid
+        if (userId != null) {
+            firestore.collection("user").document(userId).get()
+                .addOnSuccessListener { document ->
+                    if (document != null) {
+                        val role = document.getString("role")
+                        when (role) {
+                            "jamaah" -> startActivity(Intent(this, HomeActivity::class.java))
+                            "pengurus_dkm" -> startActivity(Intent(this, DkmDashboard::class.java))
+                            "admin" -> startActivity(Intent(this, AdminDashboard::class.java))
+                            else -> startActivity(Intent(this, WelcomeActivity::class.java))
+                        }
+                        finish() // agar pengguna tidak dapat kembali ke halaman welcome dengan menekan tombol back
+                    } else {
+                        // Handle the case where the document does not exist
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    // Handle the error
+                }
+        }
     }
 }
