15bd8926ccce82ff08b97afb1eac7f1de3829f18
adamrza <adamlabibmrza@gmail.com>
2024-05-27 12:52:49 +0700

    layout pantau permohonan (admin)

---
+++ app/src/main/java/org/d3if0140/masjidhub/AdminPermohonan.kt
@@ -0,0 +1,11 @@
+package org.d3if0140.masjidhub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdminPermohonan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_permohonan)
+    }
+}
