04ba41182d36423ca54ca91724261df4bff8826b
ardiyamalik <91653831+ardiyamalik@users.noreply.github.com>
2024-05-28 23:04:43 +0700

    menghubungkan button di admin dashboard ke halaman yang di tuju

--- app/src/main/java/org/d3if0140/masjidhub/AdminDashboard.kt
+++ app/src/main/java/org/d3if0140/masjidhub/AdminDashboard.kt
@@ -1,11 +1,32 @@
 package org.d3if0140.masjidhub
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.d3if0140.masjidhub.databinding.ActivityAdminDashboardBinding
 
 class AdminDashboard : AppCompatActivity() {
+    private lateinit var binding: ActivityAdminDashboardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_admin_dashboard)
+        binding = ActivityAdminDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Mengarahkan ke Halaman
+        binding.buttonMasjid.setOnClickListener{
+            startActivity(Intent(this, AdminListMasjid::class.java))
+        }
+
+        binding.buttonAgenda.setOnClickListener{
+            startActivity(Intent(this, AdminEvent::class.java))
+        }
+
+        binding.buttonKeuangan.setOnClickListener{
+            startActivity(Intent(this, AdminKeuangan::class.java))
+        }
+
+        binding.buttonAjuan.setOnClickListener{
+            startActivity(Intent(this, AdminIsiPermohonan::class.java))
+        }
     }
 }
