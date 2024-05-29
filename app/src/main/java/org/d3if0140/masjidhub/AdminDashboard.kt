ace622c8baa2c7888aa2271f2f5d4cdd6e8ad5cd
ardiyamalik <91653831+ardiyamalik@users.noreply.github.com>
2024-05-29 14:21:51 +0700

    menambahkan tombol logout (sementara)

--- app/src/main/java/org/d3if0140/masjidhub/AdminDashboard.kt
+++ app/src/main/java/org/d3if0140/masjidhub/AdminDashboard.kt
@@ -3,14 +3,30 @@ package org.d3if0140.masjidhub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import org.d3if0140.masjidhub.databinding.ActivityAdminDashboardBinding
 
 class AdminDashboard : AppCompatActivity() {
     private lateinit var binding: ActivityAdminDashboardBinding
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAdminDashboardBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Inisialisasi Firebase
+        mAuth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
+
+        // Atur listener untuk tombol logout
+        binding.buttonLogout.setOnClickListener {
+            mAuth.signOut()
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
         //Mengarahkan ke Halaman
         binding.buttonMasjid.setOnClickListener{
